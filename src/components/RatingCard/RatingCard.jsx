949d0a075226ff96434536903efbf5e21a03118e
Hemanathan H P <hphemanathan@gmail.com>
2025-03-13 12:31:24 +0530

    wip

--- src/components/RatingCard/RatingCard.jsx
+++ src/components/RatingCard/RatingCard.jsx
@@ -3,22 +3,20 @@ import { range } from '../../../util';
 
 function RatingCard() {
   return (
-    <div>
-      <form action=''>
-        <img src='src/assets/icon-star.svg' alt='orange cartoon star' />
+    <div className='rating_card'>
+      <form className='form'>
+        <div className='rating_card_image'>
+          <img src='src/assets/icon-star.svg' alt='orange cartoon star' />
+        </div>
         <h1>How did we do?</h1>
         <p>
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
 
-        {
-          range(1, 6).map((num) =>
-          (
-            <div className="rating">{num}</div>
-          
-          ))
-        }
+        {range(1, 6).map((num) => (
+          <div className='rating'>{num}</div>
+        ))}
       </form>
     </div>
   );
